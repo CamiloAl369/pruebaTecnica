905e36c12c570cb1ae4f3e115181b4538df2e6b8
Camilo Alvarez <alvarezdc777@gmail.com>
2022-11-03 19:57:18 -0500

    commit inicial

---
+++ Conexion/ConexionBD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace prueba.Conexion
+{
+    internal class ConexionBD
+    {
+        private string cadenaconexion = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=pruebas;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
+        private SqlDataAdapter adapter;
+
+        private void Conectar()
+        {
+            conn = new SqlConnection(cadenaconexion);
+
+        }
+
+        public ConexionBD()
+        {
+            Conectar();
+        }
+
+    }
+}
